827e93767961497b2035483f990d538bfd1707ec
tdnvl <bummel1889@gmail.com>
2018-03-16 12:31:15 -0400

    Use Heroku postgreSQL database.

--- routes.py
+++ routes.py
@@ -3,7 +3,11 @@ from models import db
 from forms import SignupForm
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+
+# Heroku database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ssksxhruuyeypf:d621f2ebfa613396f3f338a90c913b960dfb5b62f62eb38114887cc5c0fa7482@ec2-54-235-146-184.compute-1.amazonaws.com:5432/d561jjsn1du0hq'
+
 db.init_app(app)
 
 app.secret_key = "development-key"
